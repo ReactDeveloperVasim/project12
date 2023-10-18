15ea2a6b89f4e6b6eb247fa0f42aa2d508802ac4
ReactDeveloperVasim <reactdevloper.vasim@gmail.com>
2023-10-18 16:34:16 +0530

    proj12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import Contact from "./components/Contact";
+import Product from "./components/Product";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Navigation />
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/product" element={<Product />}/>
+      </Routes>
+    </BrowserRouter>
+    
+</>
   );
 }
 
